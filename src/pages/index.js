d895609c67c067eae8cca1481f7f717b656df91c
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-07-22 21:01:24 +0900

    ブラウザではクエリパラメータで動作

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,25 @@ import DraggableList from "../components/DraggableList";
 import arrayMove from 'array-move';
 import { InputNewRoute } from '../components/InputNewRoute';
 import { MakeRoute } from '../components/MakeRoute';
+import { useRouter } from "next/router";
 import useSWR from "swr";
 
 const Home = () => {
+  const router = useRouter();
   const [datas, setDatas] = useState([]);
   const [liff, setLiff] = useState();
-  const [msg, setMsg] = useState('');
+  const [roomID, setRoomID] = useState('');
 
   useEffect(() => {
     const liff = require('@line/liff');
-    liff.init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID }).then(() => {
-      if(!liff.isLoggedIn()){
-        liff.login();
-        setLiff(liff);
-      }
-      if(liff.isInClient()){
+    if(liff.isInClient()){
+      liff.init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID }).then(() => {
+        if(!liff.isLoggedIn()){
+          liff.login();
+        }
         const context = liff.getContext();
         const roomID =  context.roomId || context.groupId;
-        setMsg(roomID) // for debug
+        setRoomID(roomID) // for debug
         sampleData(roomID) // for debug
         db.collection('rooms').doc(roomID).collection('waypoints').get().then((snapshot) => {
           const items = [];
@@ -35,11 +36,31 @@ const Home = () => {
           });
           setDatas(items); 
         });
-      }else{
-        setMsg('undifined')
-      }
-    });
+        setLiff(liff);
+      });
+    }
   }, []);
+
+  useEffect(() => {
+    const liff = require('@line/liff');
+    if(liff.isInClient()) return;
+    if (router.asPath !== router.route) {
+      setRoomID(router.query.roomID);
+      const roomID = router.query.roomID
+      setRoomID(roomID) // for debug
+      db.collection('rooms').doc(roomID).collection('waypoints').get().then((snapshot) => {
+        const items = [];
+        snapshot.forEach((document) => {
+          const doc = document.data();
+          items.push({
+            id: document.id,
+            location_name: doc.location_name
+          });
+        });
+        setDatas(items); 
+      });
+    }
+  }, [router]);
 
   const onDrop = ({ removedIndex, addedIndex }) => {
     setDatas(arrayMove(datas, removedIndex, addedIndex));
@@ -48,7 +69,7 @@ const Home = () => {
 
   return (
     <>
-      <div>roomId : {msg}</div>
+      <div>roomId : {roomID}</div>
       <DraggableList items={datas} onDrop={onDrop} />
       <InputNewRoute />
       <MakeRoute />
