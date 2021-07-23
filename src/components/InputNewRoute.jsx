b55424a33fd5bd4e3de23b04c01e520fe5bc77b8
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-07-23 20:50:03 +0900

    現在位置取得ボタン

--- src/components/InputNewRoute.jsx
+++ src/components/InputNewRoute.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { BasicButton } from './BasicButton';
 import { db } from '../lib/firebase';
 import addData from '../lib/addData'
+import styles from "./InputNewRoute.module.css";
+import { Button } from '@material-ui/core';
+import { line } from 'cli-spinners';
 
 const roomsRef = db.collection('rooms')
 
@@ -21,21 +24,59 @@ export const InputNewRoute = (props) => {
     }
 
     // 目的地を追加
-    const addRoute = () => {
+    const addRoute = (e) => {
         if (!to) {
-            alert("新しい目的地を入力してください");
+            alert("経由地点が空欄です");
             return;
         }
         addData(props.roomID, to, props.updateDatas);
 
         deleteValue();
+        e.preventDefault();
     };
+
+    const addCurrentPos = () => {
+        const liff = require('@line/liff');
+        if(liff.isInClient()){
+            liff.init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID }).then(() => {
+                liff.getProfile().then(profile => {
+                    const name = profile.displayName;
+                    navigator.geolocation.getCurrentPosition((position) => {
+                        const pos = toString(position.coords.latitude) + toString(position.coords.longitude);
+                        addData(props.roomID, {
+                            to: pos,
+                            tag: name + "の現在位置"
+                        }, props.updateDatas);
+                    })
+                });
+            });
+        }else{
+            const name = "匿名ユーザー";
+            navigator.geolocation.getCurrentPosition((position) => {
+                const pos = String(position.coords.latitude) + ',' +  String(position.coords.longitude);
+                console.log(pos)
+                addData(props.roomID, name + "の現在位置", props.updateDatas);
+            });
+        }
+    }
 
     return (
         <>
-            <h1>tolist</h1>
-            <input value={to} onChange={handleChange} type="text" />
-            <BasicButton onClick={(to) => addRoute(to)} label={"追加する"} />
+            <div className={styles.inputcontainer}>
+                <form onSubmit={addRoute} className={styles.form}>
+                <input
+                    value={to}
+                    onChange={handleChange}
+                    className={styles.input}
+                    type="text"
+                    placeholder="追加の経由地点を入力してください"
+                />
+                </form>
+              <Button color="primary" variant="contained" onClick={addRoute}>追加する</Button>
+            </div>
+            <div className={styles.container}>
+                <Button color="primary" onClick={addCurrentPos} variant="contained">現在地を追加</Button>
+            </div>
         </>
     )
 }
