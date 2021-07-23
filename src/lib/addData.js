c469e90d94c858bd16b0c5cf1834b3aaed47a403
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-07-23 18:07:05 +0900

    addDataのトランザクション処理

--- src/lib/addData.js
+++ src/lib/addData.js
@@ -1,23 +1,30 @@
 import { db, fieldval } from "./firebase";
+import { nanoid } from "nanoid";
 
-export default async function addData(roomID, to = "つくば駅", cb){
-  db.collection('rooms').doc(roomID).collection('waypoints').add({
-    location_name: to
-  }).then((docRef) => {
-    db.collection('rooms').doc(roomID).get().then(doc => {
+export default function addData(roomID, to = "つくば駅", cb){
+  const ref = db.collection('rooms').doc(roomID);
+
+  db.runTransaction((transaction) => {
+    return transaction.get(ref).then(doc => {
+      const id = nanoid();
       if(doc.exists){
-        db.collection('rooms').doc(roomID).update({
-          order: fieldval.arrayUnion(docRef.id)
-        }).then(() => {
-          if(cb) cb();
-        });
+        transaction.update(ref, {
+          order: fieldval.arrayUnion(id)
+        })
+        transaction.set(ref.collection('waypoints').doc(id), {
+            location_name: to
+        })
       }else{
-        db.collection('rooms').doc(roomID).set({
-          order: [docRef.id]
-        }).then(() => {
-          if(cb) cb();
+        transaction.set(doc, {
+          order: [id]
+        });
+        transaction.set(ref.collection('waypoints').doc(id), {
+          location_name: to
         });
       }
-    })
-  })
+      return true
+    }) 
+  }).then(() => {
+    cb();
+  }).catch(e => console.log(e))
 }
